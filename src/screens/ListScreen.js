3261155818392fc355b688c08397917bbb6489db
Antkeo1 <sleav143@gmail.com>
2020-05-03 16:52:51 -0400

    building a list

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>List Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
